dded4dd77e3d0bd5b740405e03a64ccf6285b0ed
liuzhiyuan1 <1282587268@qq.com>
2017-09-26 11:39:26 +0800

    实现二级分类

--- mybxg/public/js/basic.js
+++ mybxg/public/js/basic.js
@@ -20,7 +20,7 @@ define([
         data : {cs_id : csId},
         url : '/api/course/basic',
         success : function (data) {
-            
+
             if(flag){
                 //添加课程
                 data.result.operate = '课程添加';
@@ -28,9 +28,32 @@ define([
                 //课程编辑
                 data.result.operate = '课程编辑';        
             }
-       console.log(data)
+      
             var html = template('basicTpl',data.result);
             $('#basicInfo').html(html);
+
+            $('#one').on('change',function(){
+                var pid = $(this).val();
+
+                $.ajax({
+                   type : 'get',
+                   datatype : 'json',
+                    data : {cg_id : pid},
+                    url : "/api/category/child",
+                    success : function (data){
+                        var tpl = '<option value="">二级分类</option>'
+                                +'{{each result}}'
+                                +'<option  {{if $index == 0}} selected {{/if}} value="{{$value.cg_id}}">{{$value.cg_name}}</option>'
+                                +'{{/each}}';
+
+                        var reader = template.compile(tpl);
+
+                        var html = reader(data);
+                        $('#two').html(html)
+                    }
+
+                })
+            })
         }
     })
 
